cfc221c062b47a9d240d87886cd9cf7920e134a4
6302177 <110083829+6302177@users.noreply.github.com>
2023-01-19 13:14:22 +0530

    Add files via upload

---
+++ CV.java
@@ -0,0 +1,26 @@
+package assgn4;
+public class CV {
+
+	public static void main(String[] args) {		     		       
+		int vCount = 0, cCount = 0,sCount=0;  
+		String str = "Vidhya@$#islearningjavafullstackby*)ineuron@#&";    
+		       str=str.toLowerCase();   
+		      for(int i = 0; i < str.length(); i++) {   
+		        if(str.charAt(i) == 'a'||str.charAt(i) == 'e'|| 
+		        		str.charAt(i) == 'i' || str.charAt(i) == 'o'|| 
+		        		str.charAt(i) == 'u') {	        	
+		                vCount++;    
+		            }
+		            else if(str.charAt(i) >= 'a' && str.charAt(i)<='z') {
+		                cCount++;    
+		            }
+		            else {
+		            	sCount++;
+		            }
+		        }    
+		        System.out.println("Number of vowels " + vCount);    
+		        System.out.println("Number of consonants " + cCount);  
+		        System.out.println("Number of specialcharecters " + sCount);    
+		    }    
+}
+
---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+package assgn4;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		
+		String s="nitin";
+		String s1="";
+		
+		for (int i=s.length()-1;i>=0;i--) {
+			s1=s1+s.charAt(i);
+		}
+		if(s.equals(s1)) {
+			System.out.println("it's a palindrome");
+		}
+		else {
+			System.out.println("it's not a palindrome");
+		}
+	}
+
+}
+
